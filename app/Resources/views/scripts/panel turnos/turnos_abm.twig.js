76365e636a8f267a1fe6344b4071ae348021e692
cristian16b <cristian.budzicz@gmail.com>
2018-10-23 17:38:18 -0300

    resuelto visualizacion panel turnos y modal modif cupo

--- app/Resources/views/scripts/panel turnos/turnos_abm.twig.js
+++ app/Resources/views/scripts/panel turnos/turnos_abm.twig.js
@@ -39,7 +39,7 @@ $(function()
 });
 
 $( function() {
-    $( "#modal-modificar-cupo" ).dialog({
+    $( "#modal-modificar-un-cupo" ).dialog({
       autoOpen: false,
       resizable: false,
       height: "auto",
@@ -67,8 +67,38 @@ $(function()
 {
     $("#boton-modificar-cupo").on("click",function()
     {
-        $("#modal-modificar-cupo").dialog('open');
-        $("#modal-modificar-cupo").dialog('option', 'title', 'Modificar cupo');
+        //pregunto si hay una fila checkeada
+        var i=0;
+        var horario,cupo;
+        //obtengo el dni y cuento cuantas filas fueron seleccionadas por el usuario
+        $("input[type=checkbox]:checked").each(function()
+        {
+            //leo el id,dni,apellido y nombre en ese orden
+            horario = $(this).parent().parent().find('td').eq(1).html();
+            cupo = $(this).parent().parent().find('td').eq(2).html();
+
+            //cuento
+            i++;
+        });
+
+        //
+        if(i === 0)
+        {
+            alert('Debe seleccionar un horario o más para actualizar el cupo');
+        }
+        else if(i > 1)
+        {
+            
+        }
+        else if(i === 1)
+        {
+            //seteos y abro
+            $("#modal-modificar-un-cupo").dialog('open');
+            $("#modal-modificar-un-cupo").dialog('option', 'title', 'Modificar cupo');
+            $("#modificar-cupo-actual").val(cupo);
+            $("#modificar-cupo-actual").val(cupo)
+        }
+        
     });
 });
 
