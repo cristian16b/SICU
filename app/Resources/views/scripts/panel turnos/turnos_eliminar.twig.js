d62a25760ee297ea9bf282880a28b487df23c20c
cristian16b <cristian.budzicz@gmail.com>
2018-10-29 18:27:15 -0300

    Inc. modales de eliminar horario & solicitante horario

--- app/Resources/views/scripts/panel turnos/turnos_eliminar.twig.js
+++ app/Resources/views/scripts/panel turnos/turnos_eliminar.twig.js
@@ -1,6 +1,5 @@
 /* 
  */
-
 $( function() {
     $( "#modal-eliminar" ).dialog({
       autoOpen: false,
@@ -10,12 +9,12 @@ $( function() {
       modal: true,
       buttons: 
       {
-        Guardar: function() 
+        Confirmar: function() 
         {
             $( this ).dialog( "close" );
         }
         ,
-        Salir: function() 
+        Cancelar: function() 
         {
            $( this ).dialog( "close" );
         }
@@ -25,4 +24,44 @@ $( function() {
     }
 //fin definicion
 );
+$(function()
+{
+    $("#boton-eliminar-horario").on("click",function()
+    {
+        $("#modal-eliminar-horario").dialog('open');
+        $("#modal-eliminar-horario").dialog('option', 'title', 'Eliminar');
+    });
+});
 
+$( function() {
+    $( "#modal-eliminar-solicitante-horario" ).dialog({
+      autoOpen: false,
+      resizable: false,
+      height: "auto",
+      width: "auto",
+      modal: true,
+      buttons: 
+      {
+        Confirmar: function() 
+        {
+            $( this ).dialog( "close" );
+        }
+        ,
+        Cancelar: function() 
+        {
+           $( this ).dialog( "close" );
+        }
+     }
+     }); 
+    //fin }
+    }
+//fin definicion
+);
+$(function()
+{
+    $("#eliminar-solicitante-horario").on("click",function()
+    {
+        $("#modal-eliminar-horario").dialog('open');
+        $("#modal-eliminar-horario").dialog('option', 'title', 'Eliminar');
+    });
+});
