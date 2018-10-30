a80948c0b9299750a5dc145c050adaf948185a99
cristian16b <cristian.budzicz@gmail.com>
2018-10-29 21:23:14 -0300

    Incorporada func. eliminar turnos y solicitud, incorporadas modalesde sendos

--- app/Resources/views/scripts/panel turnos/turnos_eliminar.twig.js
+++ app/Resources/views/scripts/panel turnos/turnos_eliminar.twig.js
@@ -11,6 +11,7 @@ $( function() {
       {
         Confirmar: function() 
         {
+            eliminarHorarios();
             $( this ).dialog( "close" );
         }
         ,
@@ -28,8 +29,33 @@ $(function()
 {
     $("#boton-eliminar-horario").on("click",function()
     {
-        $("#modal-eliminar-horario").dialog('open');
-        $("#modal-eliminar-horario").dialog('option', 'title', 'Eliminar');
+        //pregunto si hay una fila checkeada
+        var i=0;
+        var horario,cupo;
+        var lista = [];
+        //obtengo el dni y cuento cuantas filas fueron seleccionadas por el usuario
+        $(".fila-turnos:checked").each(function()
+        {
+            //leo el id,dni,apellido y nombre en ese orden
+            horario = $(this).parent().parent().find('td').eq(1).html();
+            cupo = $(this).parent().parent().find('td').eq(2).html();
+            //guardo
+            lista[i] = horario;
+
+            //cuento
+            i++;
+        });
+        if(i === 0)
+        {
+            alert('Debe seleccionar un horario o más para eliminar el cupo');
+        }
+        else if(i > 0)
+        {
+            $("#modal-eliminar-horario").dialog('open');
+            $("#modal-eliminar-horario").dialog('option', 'title', 'Eliminar');
+            //fuente: https://gist.github.com/nrojas13/bfb6edfedd9178333486b8a2b94ea46f
+            sessionStorage.setItem('listaHorarios',JSON.stringify(lista));
+        }
     });
 });
 
@@ -44,6 +70,7 @@ $( function() {
       {
         Confirmar: function() 
         {
+            eliminarHorariosSolicitantes();
             $( this ).dialog( "close" );
         }
         ,
@@ -61,7 +88,41 @@ $(function()
 {
     $("#boton-eliminar-solicitante-horario").on("click",function()
     {
-        $("#modal-eliminar-horario").dialog('open');
-        $("#modal-eliminar-horario").dialog('option', 'title', 'Eliminar');
+        var i=0;
+        var horario,cupo;
+        var lista = [];
+        //obtengo el dni y cuento cuantas filas fueron seleccionadas por el usuario
+        $(".fila-turnos:checked").each(function()
+        {
+            //leo el id,dni,apellido y nombre en ese orden
+            horario = $(this).parent().parent().find('td').eq(1).html();
+            cupo = $(this).parent().parent().find('td').eq(2).html();
+            //guardo
+            lista[i] = horario;
+
+            //cuento
+            i++;
+        });
+        if(i === 0)
+        {
+            alert('Debe seleccionar una solicitud o más para eliminar');
+        }
+        else if(i > 0)
+        {
+            $("#modal-eliminar-solicitante-horario").dialog('open');
+            $("#modal-eliminar-solicitante-horario").dialog('option', 'title', 'Eliminar');
+            //fuente: https://gist.github.com/nrojas13/bfb6edfedd9178333486b8a2b94ea46f
+            sessionStorage.setItem('listaHorarios',JSON.stringify(lista));
+        }
     });
 });
+
+function eliminarHorarios()
+{
+    //
+}
+
+function eliminarHorariosSolicitantes()
+{
+    //
+}
