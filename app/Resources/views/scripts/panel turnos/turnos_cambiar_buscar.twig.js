9fc6fed6b07b03537dcd123b9aa3a0afae93be41
cristian16b <cristian.budzicz@gmail.com>
2018-10-31 20:37:42 -0300

    Incorp. modif. modal solicitante-turno,to-do terminar js y hacer func. php correspondientes

--- app/Resources/views/scripts/panel turnos/turnos_cambiar_buscar.twig.js
+++ app/Resources/views/scripts/panel turnos/turnos_cambiar_buscar.twig.js
@@ -29,8 +29,34 @@ $(function()
 {
     $("#boton-cambiar-turno").on("click",function()
     {
-        $("#modal-solicitante-turno").dialog('open');
-        $("#modal-solicitante-turno").dialog('option', 'title', 'Cambiar turno');
+        var i=0;
+        var dni,apellidoNombre,tipo;
+        //obtengo el dni y cuento cuantas filas fueron seleccionadas por el usuario
+        $(".fila-solicitantes:checked").each(function()
+        {
+            //guardo
+            dni = $(this).parent().parent().find('td').eq(1).html();
+            apellidoNombre = $(this).parent().parent().find('td').eq(2).html();
+            tipo = $(this).parent().parent().find('td').eq(3).html();
+            //cuento
+            i++;
+        });
+        if(i === 1)
+        {
+            //seteo los inputs
+            $("#nombre-apellido-solicitante").val(apellidoNombre);
+            $("#dni-solicitante").val(dni);
+            $("#tipo-comensal-solicitante").val(tipo);
+            
+            
+            //abro
+            $("#modal-solicitante-turno").dialog('open');
+            $("#modal-solicitante-turno").dialog('option', 'title', 'Cambiar turno');
+        }
+        else if(i > 0 || i === 0 )
+        {
+            alert('Solo se puede cambiar un turno a la vez.');
+        }
     });
 });
 
