36cd3efebc7ee5dcea950aa68ad40be6a99ee7be
cristian16b <cristian.budzicz@gmail.com>
2018-11-23 18:52:18 -0300

    Completado resumen de tarjetas

--- app/Resources/views/scripts/panel tarjetas/tarjetas_resumen.twig.js
+++ app/Resources/views/scripts/panel tarjetas/tarjetas_resumen.twig.js
@@ -15,20 +15,74 @@ $( function() {
      });
     //evento para abrir la modal
     $("#boton-resumen-tarjeta").on("click",function()
-    {
-        $("#modal-resumen-tarjeta").dialog('open');
-        $("#modal-resumen-tarjeta").dialog('option', 'title', 'Resumen rapido');
+    { 
+       //reseteo el select
+       var select = $('#select-organismo');
+       select.val($('option:first', select).val());
+        
+       //seteo los inputs
+       $("#input-cantidad-tarjeta").val(0);
+       $("#input-saldo-positivo-tarjeta").val(0);
+       $("#input-saldo-negativo-tarjeta").val(0);
+        
+       //abro la modal 
+       $("#modal-resumen-tarjeta").dialog('open');
+       $("#modal-resumen-tarjeta").dialog('option', 'title', 'Resumen rapido');
     });
     
     //evento cambio del select
     $("#select-organismo").on("change",function(){
-        alert('cambio'); 
+       
+       datos = {};
+       datos.organismo = $("#select-organismo").val();
+       
+       if(organismo !== 'Facultad y/o Secretaría')
+       {
+            $.ajax
+                ({
+                    async:true,
+                    method: 'GET',
+                    url: "{{ path('tarjetas_resumen') }}",
+                    data: datos,
+                    dataType: 'json',
+                    beforeSend: function()
+                    {
+                        $.blockUI({ message: '<img src="/img/cargando.gif"><h3>Cargando ...</h3>' });  
+                    },
+                    success: cargarInformacionResumen,
+                    timeout:12500,
+                    error : function() 
+                    {
+                        //desbloqueo la pagina
+                        $.unblockUI();
+                        alert('ERROR DE CONEXIÓN, INTENTE NUEVAMENTE MAS TARDE');
+                    }
+                });
+       }
     });
-    
     //fin funcion anonima
 });
 
 function cargarInformacionResumen(datos)
 {
-    
+    $.unblockUI();
+    if(datos.length > 0)
+    {
+        var cantidad = datos[0].cantidad;
+        var saldoPositivo =  datos[1].saldoPositivo;
+        var saldoNegativo = datos[2].saldoNegativo;
+        //seteo los inputs
+        if(cantidad !== null)
+        {
+            $("#input-cantidad-tarjeta").val(cantidad);
+        }
+        if(cantidad !== null)
+        {
+            $("#input-saldo-positivo-tarjeta").val(saldoPositivo);
+        }
+        if(cantidad !== null)
+        {
+            $("#input-saldo-negativo-tarjeta").val(saldoNegativo);
+        }
+    }
 }
--- src/ComensalesBundle/Controller/ResumenTarjetaController.php
+++ src/ComensalesBundle/Controller/ResumenTarjetaController.php
@@ -22,7 +22,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  * + saldo negativo ( deudas de los comensales)
  * @author Cristian B
  */
-class ResumenTarjetaController {
+class ResumenTarjetaController extends Controller{
     
    /**
    * @Route("tarjetas/resumen",name="tarjetas_resumen")     
@@ -30,6 +30,64 @@ class ResumenTarjetaController {
    */
    public function obtenerResumenInformativo(Request $request)
    {
+       $organismo = $request->query->get('organismo');
        
+       $resultado = null;
+       if(isset($organismo))
+       {
+            $resultado = array(
+                    $this->obtenerCantidad($organismo),
+                    $this->obtenerSaldoPositivo($organismo),
+                    $this->obtenerSaldoNegativo($organismo)
+            );
+       }
+       return new JsonResponse($resultado);
+   }
+   
+   private function obtenerSaldoPositivo($organismo)
+   {
+       $qb = $this->getDoctrine()->getEntityManager()->createQueryBuilder()
+                   ->select('sum(tarj.saldo) as saldoPositivo')
+                   ->from('ComensalesBundle:Tarjeta','tarj')
+                   ->innerJoin('tarj.estadoTarjeta','est')
+                   ->innerJoin('tarj.solicitud','soli')
+                   ->innerJoin('soli.persona','per')
+                   ->innerJoin('per.facultad','facu')
+                   ->where('facu.nombreFacultad = :organismo')
+                   ->andWhere('tarj.saldo > 0')
+                   ->setParameter('organismo',$organismo)
+            ;
+        return $qb->getQuery()->getSingleResult();
+   }
+   
+   private function obtenerSaldoNegativo($organismo)
+   {
+       $qb = $this->getDoctrine()->getEntityManager()->createQueryBuilder()
+                   ->select('sum(tarj.saldo) as saldoNegativo')
+                   ->from('ComensalesBundle:Tarjeta','tarj')
+                   ->innerJoin('tarj.estadoTarjeta','est')
+                   ->innerJoin('tarj.solicitud','soli')
+                   ->innerJoin('soli.persona','per')
+                   ->innerJoin('per.facultad','facu')
+                   ->where('facu.nombreFacultad = :organismo')
+                   ->andWhere('tarj.saldo <= 0')
+                   ->setParameter('organismo',$organismo)
+            ;
+        return $qb->getQuery()->getSingleResult();
+   }
+   
+   private function obtenerCantidad($organismo)
+   {
+       $qb = $this->getDoctrine()->getEntityManager()->createQueryBuilder()
+                   ->select('count(tarj) as cantidad')
+                   ->from('ComensalesBundle:Tarjeta','tarj')
+                   ->innerJoin('tarj.estadoTarjeta','est')
+                   ->innerJoin('tarj.solicitud','soli')
+                   ->innerJoin('soli.persona','per')
+                   ->innerJoin('per.facultad','facu')
+                   ->where('facu.nombreFacultad = :organismo')
+                   ->setParameter('organismo',$organismo)
+            ;
+        return $qb->getQuery()->getSingleResult();
    }
 }
