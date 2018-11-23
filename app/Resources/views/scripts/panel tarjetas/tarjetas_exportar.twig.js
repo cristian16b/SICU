d44fc46345b7f9e83d4a67200181a20653120f9d
cristian16b <cristian.budzicz@gmail.com>
2018-11-23 19:36:46 -0300

    Incorp exportacion pdf y xls,refactorizado turnos_exportar

--- app/Resources/views/scripts/panel tarjetas/tarjetas_exportar.twig.js
+++ app/Resources/views/scripts/panel tarjetas/tarjetas_exportar.twig.js
@@ -1,7 +1,37 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(function(){
+    $("#boton-exportar-tarjetas-xls").on("click",function() 
+    {
+        var organismo =  $('#organismo').val();
+        var tipoFiltro = $('#tipo-filtro').val();
+        if(tipoFiltro !== null && organismo !== null)
+        {
+             var sFileName = "Listado de tarjetas  - " + tipoFiltro  + ' - ' + organismo ;
+            $('#tablaTarjetas').tableExport({type:'excel',
+                fileName: sFileName,
+                ignoreColumn: [0,7]});
+        }
+    });
+    $("#boton-exportar-tarjetas-pdf").on("click",function() 
+    {
+        var organismo =  $('#organismo').val(); 
+        var tipoFiltro = $('#tipo-filtro').val();
+        if(tipoFiltro !== null && organismo !== null)
+        {
+            var sFileName = "Listado de tarjetas  - " + tipoFiltro  + ' - ' + organismo ;
+            function DoCellData(cell, row, col, data) {}
+            function DoBeforeAutotable(table, headers, rows, AutotableSettings) {}
+            //alert(facultad);
+            $('#tablaTarjetas').tableExport({fileName: sFileName,
+                            type: 'pdf',
+                            ignoreColumn: [0,7],
+                            jspdf: {format: 'bestfit',
+                                    margins: {left:20, right:10, top:20, bottom:20},
+                                    autotable: {styles: {overflow: 'linebreak'},
+                                                tableWidth: 'wrap',
+                                                tableExport: {onBeforeAutotable: DoBeforeAutotable,
+                                                              onCellData: DoCellData}}}
+                           });
+        }
+    });
+});
+ 
