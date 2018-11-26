a6c15da048edc5d322cbc227944c4230b7a21ad0
cristian16b <cristian.budzicz@gmail.com>
2018-11-26 16:50:52 -0300

    Incorp archivo historial

---
+++ src/ComensalesBundle/Controller/HistorialCRController.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace ComensalesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+/**
+ * Description of HistorialTarjetaController
+ * @Route("/historial",name="historial_")
+ * @author Cristian B
+ */
+class HistorialCRController extends Controller
+{
+    public function obtenerHistorialTarjeta(Request $request)
+    {
+        
+    }
+    
+    public function obtenerMenusConsumidos(Request $request)
+    {
+        
+    }
+    
+    public function obtenerRecargas(Request $request)
+    {
+        
+    }
+}
