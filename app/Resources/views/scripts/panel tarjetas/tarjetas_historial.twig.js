54e29721fc2c3be01ddbe991b7d330ee79399941
cristian16b <cristian.budzicz@gmail.com>
2018-11-27 16:56:19 -0300

    Incorp modif a la modal de historial, refactor funciones de carga historial en js

--- app/Resources/views/scripts/panel tarjetas/tarjetas_historial.twig.js
+++ app/Resources/views/scripts/panel tarjetas/tarjetas_historial.twig.js
@@ -5,14 +5,12 @@ $(function()
         var id = $(this).parents('TR').find('TD').eq(1).html();
         var nombreApellido = $(this).parents('TR').find('td').eq(3).html();
         var saldo = $(this).parents('TR').find('td').eq(6).text();
-//        mostrarHistorial(id);
         //muestro la modal
         $("#input-nombre-comensal-tarjeta").val(nombreApellido);
-        $("#input-saldo-tarjeta").val(saldo)
-        
+        $("#input-saldo-tarjeta").val(saldo);
+        $("#id-tarjeta").val(id);
         $("#modal-historial-tarjetas").dialog('open');
         $("#modal-historial-tarjetas").dialog('option', 'title', 'Historial Consumos y Recargas');
-        
     });
    
    $('#tablaHistorial').DataTable({
@@ -41,84 +39,70 @@ $(function()
                   "sSortDescending": ": Activar para ordenar la columna de manera descendente"
               }
    }});
+   
+   $("#anio").on("change",function()
+   {
+       var anio =  $("#anio").val();
+       var tipoHistorial =  $("#tipo-historial").val();
+       var id = $("#id-tarjeta").val();
+       mostrarHistorial(id,tipoHistorial,anio);
+   });
 });
 
-function mostrarHistorial(id)
+function mostrarHistorial(id,tipoHistorial,anio)
 {
-//    datos = {};
-//    datos.id = id;
-//    $.ajax
-//    ({
-//        async:true,
-//        method: 'GET',
-//        url: "{{ path('tarjetas_historial') }}",
-//        data: datos,
-//        dataType: 'json',
-//        beforeSend: function()
-//        {
-//            $.blockUI({ message: '<img src="/img/cargando.gif"><h3>Cargando ...</h3>' });  
-//        },
-//        success: cargarFilasHistorial,
-//        timeout:12500,
-//        error : function() 
-//        {
-//            //desbloqueo la pagina
-//            $.unblockUI();
-//            alert('ERROR DE CONEXIÓN, INTENTE NUEVAMENTE MAS TARDE');
-//        }
-//    });
+    datos = {};
+    datos.id = id;
+    datos.tipoHistorial = tipoHistorial;
+    datos.anio = anio;
+    $.ajax
+    ({
+        async:true,
+        method: 'GET',
+        url: "{{ path('tarjetas_historial') }}",
+        data: datos,
+        dataType: 'json',
+        beforeSend: function()
+        {
+            $.blockUI({ message: '<img src="/img/cargando.gif"><h3>Cargando ...</h3>' });  
+        },
+        success: cargarFilasHistorial,
+        timeout:12500,
+        error : function() 
+        {
+            //desbloqueo la pagina
+            $.unblockUI();
+            alert('ERROR DE CONEXIÓN, INTENTE NUEVAMENTE MAS TARDE');
+        }
+    });
 }
 
 function cargarFilasHistorial(datos)
-{
-    
-    
-//    var tabla = $('#tablaTarjetas').DataTable();
-//    var id,fechaAlta,saldo,estado,nombre,apellido,dni,nombreApellido,check,fecha;
-//    var i;
-//    var tamanio = datos.length;
-//    for(i= 0;i < tamanio; i++)
-//    {
-//        fechaAlta = '<td>'+datos[i].fechaAlta.date+'</td>';
-//        estado = '<td>'+datos[i].nombreEstadoTarjeta+'</td>';
-//        nombre = '<td>'+datos[i].nombre+'</td>';
-//        apellido = '<td>'+datos[i].apellido+'</td>'; 
-//        dni = '<td>'+datos[i].dni+'</td>';
-//        id = '<td>'+datos[i].id+'</td>';
-//        saldotarj = datos[i].saldo;
-//        nombreApellido = apellido + ' , ' + nombre;
-//        boton = '<td> <input class="boton-ver-historial btn btn-info btn-sm"  type="button" value=">>" /></td>';
-//        check = '<td><input type="checkbox" class=' + '"form-control fila-tarjetas"' + '/></td>';
-//
-//        if(saldotarj > 0)
-//        {
-//            saldo = '<td><b>'+saldotarj+'</b></td>';
-//        }
-//        else
-//        {
-//            saldo = '<td><b style="color:red">'+saldotarj+'</b></td>';
-//        }
-//        
-//        if(fechaAlta === null)
-//        {
-//            fechaAlta = 'No entregada';
-//        }
-//        else
-//        {
-//            fecha = fechaAlta.split(' ');
-//        }
-//        
-//
-//        tabla.row.add( 
-//                [
-//                    check,
-//                    id,
-//                    dni,
-//                    nombreApellido,
-//                    estado,
-//                    fecha[0],
-//                    saldo,
-//                    boton
-//                ]).draw(false);
-//    }
+{     
+    var tabla = $('#tablaTarjetas').DataTable();
+    var fecha,sede,concepto,monto,saldo;
+    var i;
+    var tamanio = datos.length;
+    for(i= 0;i < tamanio; i++)
+    {
+        fecha     = '<td>'+datos[i].fecha.date+'</td>';
+        saldotarj =        datos[i].saldo;
+        concepto  = '<td>'+datos[i].concepto+'</td>';
+        monto     = '<td>'+datos[i].monto+'</td>';
+        saldo     = '<td>'+datos[i].saldo+'</td>';
+
+        if(saldotarj > 0)
+        {
+            saldo = '<td><b>'+saldotarj+'</b></td>';
+        }
+        else
+        {
+            saldo = '<td><b style="color:red">'+saldotarj+'</b></td>';
+        }
+        
+        tabla.row.add( 
+                [
+                    fecha,sede,concepto,monto,saldo
+                ]).draw(false);
+    }
 }
