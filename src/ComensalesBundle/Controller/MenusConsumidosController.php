14c884638e71dccc5ba590b62640c9b7e936f6c6
cristian16b <cristian.budzicz@gmail.com>
2018-11-28 17:07:01 -0300

    Incorp clases vacias para menus

---
+++ src/ComensalesBundle/Controller/MenusConsumidosController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace ComensalesBundle\Controller;
+
+/**
+ * Description of MenusConsumidosController
+ *
+ * @author Cristian B
+ */
+class MenusConsumidosController {
+    //put your code here
+}
