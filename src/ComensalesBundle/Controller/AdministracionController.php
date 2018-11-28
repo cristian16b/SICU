35359a5c4cc120840f8cae5400fc8704bead0458
cristian16b <cristian.budzicz@gmail.com>
2018-11-28 17:55:59 -0300

    Incorp filtros en el template

--- src/ComensalesBundle/Controller/AdministracionController.php
+++ src/ComensalesBundle/Controller/AdministracionController.php
@@ -8,11 +8,30 @@
 
 namespace ComensalesBundle\Controller;
 
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 /**
  * Description of AdministracionController
- *
+ * @Route("/administracion",name="administracion_")
  * @author Cristian B
  */
-class AdministracionController {
-    //put your code here
+
+class AdministracionController extends Controller{
+    
+    /**
+     * @Route("/panel",name="panel")
+     */
+    public function mostrarPanel()
+    {
+        $organismos = $this->container->get('organismos')->obtenerOrganismos();
+        return $this->render('Panel menus consumidos/panelMenusConsumidos.html.twig',
+                array('organismos' => $organismos,
+                     ));
+    }
 }
