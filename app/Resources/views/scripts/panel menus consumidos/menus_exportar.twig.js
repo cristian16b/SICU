a7d8569ab4227d60acd04c242f28f98b836fb505
cristian16b <cristian.budzicz@gmail.com>
2018-12-01 20:31:07 -0300

    lograda exportacion a xls de sendas tablas,revisar exporta a pdf

---
+++ app/Resources/views/scripts/panel menus consumidos/menus_exportar.twig.js
@@ -0,0 +1,116 @@
+$(function(){
+    $("#boton-exportar-ventas-xls").on("click",function() 
+    {
+        var sede =  $('#organismo').val();
+        var fechaInicio = $('#fecha-inicio').val();
+        var fechaFin = $('#fecha-fin').val();
+        if(fechaInicio !== null && sede !== null)
+        {
+            //veo si la fecha fin no es nulla, eso impacta en el nombre del archivo
+            var sFileName;
+            if(fechaFin !== '')
+            {
+                alert('entra aca');
+                sFileName = "Listado de ventas  - " + sede  + ' -  Del ' + fechaInicio + ' - Al ' + fechaFin ;
+            }
+            else
+            {
+                alert('va por el else');
+                sFileName = "Listado de ventas  - " + sede  + ' - ' + fechaInicio  ;
+            }
+            $('#tablaRecargas').tableExport({type:'excel',
+                fileName: sFileName
+            });
+        }
+    });
+    $("#boton-exportar-ventas-pdf").on("click",function() 
+    {
+        var sede =  $('#organismo').val();
+        var fechaInicio = $('#fecha-inicio').val();
+        var fechaFin = $('#fecha-fin').val();
+        alert(fechaFin);
+        if(fechaInicio !== null && sede !== null)
+        {
+            //veo si la fecha fin no es nulla, eso impacta en el nombre del archivo
+            var sFileName ='ss';
+//            if(fechaFin === null)
+//            {
+//                sFileName = "Listado de ventas  - " + sede  + ' -  Del ' + fechaInicio + ' - Al ' + fechaFin ;
+//            }
+//            else
+//            {
+//                sFileName = "Listado de ventas  - " + sede  + ' - ' + fechaInicio  ;
+//            }
+            
+            function DoCellData(cell, row, col, data) {}
+            function DoBeforeAutotable(table, headers, rows, AutotableSettings) {}
+            //alert(facultad);
+            $('#tablarRecargas').tableExport({fileName: sFileName,
+                            type: 'pdf',
+                            jspdf: {format: 'bestfit',
+                                    margins: {left:20, right:10, top:20, bottom:20},
+                                    autotable: {styles: {overflow: 'linebreak'},
+                                                tableWidth: 'wrap',
+                                                tableExport: {onBeforeAutotable: DoBeforeAutotable,
+                                                              onCellData: DoCellData}}}
+                           });
+        }
+    });
+    ///////////////////////////////////////////////////////////////////////////////
+//
+   $("#boton-exportar-consumos-xls").on("click",function() 
+    {
+        var sede =  $('#organismo').val();
+        var fechaInicio = $('#fecha-inicio').val();
+        var fechaFin = $('#fecha-fin').val();
+        if(fechaInicio !== null && sede !== null)
+        {
+            //veo si la fecha fin no es nulla, eso impacta en el nombre del archivo
+            var sFileName;
+            if(fechaFin === null)
+            {
+                sFileName = "Listado de ventas  - " + sede  + ' -  Del ' + fechaInicio + ' - Al ' + fechaFin ;
+            }
+            else
+            {
+                sFileName = "Listado de ventas  - " + sede  + ' - ' + fechaInicio  ;
+            }
+            $('#tablaConsumos').tableExport({type:'excel',
+                fileName: sFileName
+            });
+        }
+    });
+    $("#boton-exportar-consumos-pdf").on("click",function() 
+    {
+        var sede =  $('#organismo').val();
+        var fechaInicio = $('#fecha-inicio').val();
+        var fechaFin = $('#fecha-fin').val();
+        if(fechaInicio !== null && sede !== null)
+        {
+            //veo si la fecha fin no es nulla, eso impacta en el nombre del archivo
+            var sFileName;
+            if(fechaFin === null)
+            {
+                sFileName = "Listado de consumos  - " + sede  + ' -  Del ' + fechaInicio + ' - Al ' + fechaFin ;
+            }
+            else
+            {
+                sFileName = "Listado de consumos - " + sede  + ' - ' + fechaInicio  ;
+            }
+            
+            function DoCellData(cell, row, col, data) {}
+            function DoBeforeAutotable(table, headers, rows, AutotableSettings) {}
+            //alert(facultad);
+            $('#tablarConsumos').tableExport({fileName: sFileName,
+                            type: 'pdf',
+                            jspdf: {format: 'bestfit',
+                                    margins: {left:20, right:10, top:20, bottom:20},
+                                    autotable: {styles: {overflow: 'linebreak'},
+                                                tableWidth: 'wrap',
+                                                tableExport: {onBeforeAutotable: DoBeforeAutotable,
+                                                              onCellData: DoCellData}}}
+                           });
+        }
+    }); 
+    
+});
